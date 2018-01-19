37e6d2647c0e8841948f491e6b4ab9181221218f
luzengAdelaide <sophiezenglu@126.com>
2018-01-19 17:10:23 +1030

    add ncbi-blast for ReportsJ

--- Codes/reportsJ.sh
+++ Codes/reportsJ.sh
@@ -1,22 +1,28 @@
 #!/bin/bash
 
-# Filter protein data (Uniprot database)
+#######################################################
+# Parameters meaning
+# gspmax: max. number of gapped HSPs (GSPs) saved per subject sequence (default 0; 0 => unlimited)
+# -B -V: the B and V options limit the number of subject sequences for which any results whatsoever are reported, regardless of the number of HSPs or GSPs found. 
+# -E: Expectation value (E) threshold for saving hits 
+# -cpus: no. of processors to utilize on multi-processor systems
+#######################################################
+
+
+# Search protein data (Uniprot database)
+# If you decided to download your own datasets, you need to make database for the blastx
+xdformat -p -k uniprot_sprot.fasta
 blastx ./report_run/sprot notKnown.fa  -gspmax=1 -E 0.00001 -B 1 -V 1 -cpus=32 > notKnown.fa.spwb
 python ./report_run/wublastx2gff.py notKnown.fa.spwb > notKnown.fa.spwb.gff
-python ./report_run/gff_pretty.py notKnown.fa.spwb.gff > notKnown.fa.spwb.txt
-report_./run/piler -annot notKnown.fa.gff -rep notKnown.fa.spwb.gff -out notKnown.fa.spwbannot.gff
-python ./report_run/gff_pretty.py notKnown.fa.spwbannot.gff > notKnown.fa.spwbannot.txt
 
-# Identify from GB_TE databae
+# Search from GB_TE database
+# If you decided to download your own datasets, you need to make database for the blastx
+xdformat -p -k GB_TE.21032016.fa -o GB_TE.new
 blastx ./BlastDB/GB_TE.new notKnown.fa -gspmax=1 -E 0.00001 -B 1 -V 1 -cpus=32 > notKnown.fa.tewb
 python ./report_run/wublastx2gff.py notKnown.fa.tewb > notKnown.fa.tewb.gff
-python ./report_run/gff_pretty.py notKnown.fa.tewb.gff > notKnown.fa.tewb.txt
-./report_run/piler -annot notKnown.fa.gff -rep notKnown.fa.tewb.gff -out notKnown.fa.tewbannot.gff
-python ./report_run/gff_pretty.py notKnown.fa.tewbannot.gff > notKnown.fa.tewbannot.txt
 
-# Filter Retrovirus data
+# Search Retrovirus data
+# If you decided to download your own datasets, you need to make database for the tblastx
+xdformat -p -k all_retrovirus.fasta
 tblastx ./BlastDB/all_retrovirus.fasta notKnown.fa -gspmax=1 -E 0.00001 -B 1 -V 1 -cpus=32 > notKnown.fa.ervwb
 python ./report_run/wublastx2gff.py notKnown.fa.ervwb > notKnown.fa.ervwb.gff
-python ./report_run/gff_pretty.py notKnown.fa.ervwb.gff > notKnown.fa.ervwb.txt
-./report_run/piler -annot notKnown.fa.gff -rep notKnown.fa.ervwb.gff -out notKnown.fa.ervwbannot.gff
-python ./report_run/gff_pretty.py notKnown.fa.ervwbannot.gff > notKnown.fa.ervwbannot.txt
---
+++ Codes/untitled.java
@@ -0,0 +1,158 @@
+import java.io.*;
+import java.util.*;
+
+/*************************
+ClassifyConsensusSequences 
+	Classifies sequences based on their censor output using the mam and our_known_reps_20130520.fasta libraries
+	Author: Joy Raison
+	Data: September 4, 2014
+	Updated: September 5, 2014 To include our_known_reps_20130520.fasta library
+	Updated:September 9, 2014 To print the notKnown.fa.gff file
+	Inputs: The consensus sequences (fasta format)
+		The repbase mam library (fasta format)
+		The our_known_reps library (fasta format)
+		The map file from the censor run
+	Outputs: check.txt (for checking individual sequence mappings)
+		known.txt (list of "identified" sequences with name of the library sequence they match)
+		partial.txt (list of partially matched sequences with a name list of the library sequences they match)
+		notKnown.fa (fasta file of the not known sequences, including seqs with hits to CENSOR or not) 
+		notKnown.fa.gff (only include family names have hit(s) with CENSOR) 		
+***************************/
+
+
+public class ClassifyConsensusSequences {
+	private static String lFile = "./Vertebrate_use.fa";
+	private static String bDir = "./";
+	private static String dDir = bDir + "results_classify/";
+	private static String cFile = bDir + "ConsensusSequences.fa";
+	private static String map = bDir + "ConsensusSequences.fa.map";
+	private static String olFile = "./our_known_reps_20130520.fasta";
+	private static String known = dDir + "known.txt";
+	private static String partial = dDir + "partial.txt";
+	private static String check = dDir + "check.txt";
+	private static String notKnown = dDir + "notKnown.fa";
+	private static String gff = dDir + "notKnown.fa.gff";
+
+	public static void main (String[] args) {
+		Hashtable<String, Integer> cLengths = getLengths(cFile);
+		String[] libs = {lFile, olFile};
+		Hashtable<String, Integer> libLengths = getLibraryLengths(libs);
+		Set<String> knowns = classify (map, cLengths, libLengths, known);
+		printNotKnownSequences (cFile, knowns, notKnown);
+		}
+
+
+	private static Set<String> classify (String mapFile, Hashtable<String, Integer> btL, 
+		Hashtable<String, Integer> libL, String kOutFile, String pOutFile, String cOutFile, String gffFile) {
+		Hashtable<String, String> kMaps = new Hashtable<String, String>();
+		try {
+			fferedReader in = new BufferedReader(new FileReader(map));
+			while ((line = in.readLine()) != null) processMapping(btL, libL, kMaps, line);
+			in.close();
+			}
+		catch (IOException ie) {ie.printStackTrace();}
+		System.out.println("" + kMaps.size() + " consensus families have known mapping");
+		printMappings(kMaps, kOutFile);
+		return kMaps.keySet();
+		}
+
+	private static Hashtable<String, Integer> getLengths (String file) {
+		Hashtable<String, Integer> lengths = new Hashtable<String, Integer>();
+		try {
+			BufferedReader in = new BufferedReader(new FileReader(file));
+			int count = 0;
+			String line = null;
+			String id = null;
+			StringTokenizer st = null;
+			while ((line = in.readLine()) != null) 
+				if (line.length() > 0)
+					if (line.charAt(0) == '>') {
+						if (count > 0) {
+							lengths.put(id, new Integer(count));
+							count = 0;
+							}
+						st = new StringTokenizer(line);
+						id = st.nextToken().substring(1);
+						}
+					else count += line.length();
+			lengths.put(id, new Integer(count));
+			in.close();
+			}
+		catch (IOException ie) {ie.printStackTrace();}
+		System.out.println("There are " + lengths.size() + " sequence lengths");
+		return lengths;
+		}
+
+	private static Hashtable<String, Integer> getLibraryLengths (String[] libs) {
+		Hashtable<String, Integer> lengths = new Hashtable<String, Integer>();
+		for (int i=0; i<libs.length; i++) {
+			Hashtable<String, Integer> fileLengths = getLengths(libs[i]);
+			for (Enumeration<String> keys=fileLengths.keys(); keys.hasMoreElements();) {
+				String key = keys.nextElement();
+				lengths.put(key, fileLengths.get(key));
+				}
+			}
+		System.out.println("There are " + lengths.size() + " library sequence lengths");
+		return lengths;
+		}
+		
+	private static void printMappings (Hashtable<String, String> map, String outFile) {
+		try {
+			BufferedWriter out = new BufferedWriter(new FileWriter(outFile));
+			out.write("Sequence MappedTo\n");
+			String key = null;
+			for (Enumeration<String> e= map.keys(); e.hasMoreElements();) {
+				key = e.nextElement();
+				out.write(key + " " + map.get(key) + "\n");
+				}
+			out.close();
+			}
+		catch (IOException ie) {ie.printStackTrace();}
+		}
+
+
+	private static void printNotKnownSequences(String seqFile, Set<String> notWanted, String outFile) {
+		try {
+			BufferedWriter out = new BufferedWriter(new FileWriter(outFile));
+			BufferedReader in = new BufferedReader(new FileReader(seqFile));
+			boolean wanted = false;
+			String line = null;
+			while ((line = in.readLine()) != null) {
+				if (line.charAt(0) == '>') wanted = !notWanted.contains(line.substring(1).split(" ")[0]);
+				if (wanted) out.write(line + "\n");
+				}
+			in.close();
+			out.close();
+			}
+		catch (IOException ie) {ie.printStackTrace();}
+		}
+						
+	private static void processMapping (Hashtable<String, Integer> btL, Hashtable<String, Integer> libL,
+		Hashtable<String, String> kMapsString line) {
+		StringTokenizer st = new StringTokenizer(line);
+		String bt = st.nextToken();
+		try {
+			int bStart = Integer.parseInt(st.nextToken());
+			int bEnd = Integer.parseInt(st.nextToken());
+			String lib = st.nextToken();
+			int lStart = Integer.parseInt(st.nextToken());
+			int lEnd = Integer.parseInt(st.nextToken());
+			int bl = 0;
+			if (btL.containsKey(bt)) bl = btL.get(bt).intValue();
+			else System.out.println("Length of " + bt + " could not be found");
+			int ll = 0;
+			if (libL.containsKey(lib)) ll = libL.get(lib).intValue();
+			else System.out.println("Length of " + lib + " could not be found");
+			if (bl > 0 && ll > 0) {
+				int bpc = Math.round (100f * (bEnd - bStart + 1) / bl);
+				int lspc = Math.round (100f * lStart / ll);
+				int lepc = Math.round (100f * lEnd / ll);
+				cOut.write(bt + " " + bStart + " " + bEnd + " (" + bpc + ") " + lib + " "  + lStart + " " + lEnd +
+					" (" + lspc + "," + lepc + ")\n");
+				if (bpc >= 85 && lspc <= 5 && lepc >= 95) kMaps.put(bt, lib); 	 
+				} 
+		catch (NumberFormatException ne) {System.out.println("Could not parse: " + line);} 
+		catch (IOException ie) {ie.printStackTrace();}
+		}
+
+	
